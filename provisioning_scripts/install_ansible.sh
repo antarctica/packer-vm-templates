c013ac45f0fe77f92fd915944c5402b75cc2869c
Felix Fennell <felnne@bas.ac.uk>
2014-07-12 21:00:50 +0100

    Now using Ansible templated files
    This replaces the awkward /etc/default/locale setting command to be a much nicer template file.

--- provisioning_scripts/install_ansible.sh
+++ provisioning_scripts/install_ansible.sh
@@ -13,3 +13,7 @@ git clone git://github.com/ansible/ansible.git /home/vagrant/ansible_installer
 cd /home/vagrant/ansible_installer
 make install
 rm -rf /home/vagrant/ansible_installer
+
+# Create a directory for Ansible's files
+mkdir -p /tmp/packer-provisioner-ansible-local/files
+chmod 777 -R  /tmp/packer-provisioner-ansible-local
