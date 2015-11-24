e7062bceae5ae8b24d1d59994aef95d077d40179
Felix Fennell <felnne@bas.ac.uk>
2015-11-24 09:37:39 +0000

    Enabling system firewall by default
    This is to ensure CentOS and Ubuntu remain consistent where possible in terms of policy.
    
    As this is a breaking change, a new major release is needed (3.0.0).
    
    A script is used to enable the firewall and allow SSH for both desktop and cloud images.

---
+++ provisioning/scripts/ubuntu/ufw.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -eux
+
+# Ensure firewall exists
+sudo apt-get install ufw;
+
+# Allow SSH traffic for remote management
+sudo ufw allow OpenSSH;
+
+# Start and enable firewall
+sudo ufw --force enable;
